6d22f64f3516c1c56fa3eebf9bcfadc930e2f80d
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-18 00:53:19 +0100

    add response and server request from example

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var documentClient = require("documentdb").DocumentClient;
 var url = require('url');
 var restify = require('restify');
 var dotenv = require("dotenv");
+var bodyParser = require('body-parser');
+const request = require('request');
+
 dotenv.config();
 
 var express = require('express');
@@ -19,7 +22,8 @@ var collectionUrl = `${databaseUrl}/colls/${process.env.COSMOS_COLLECTION_ID}`;
 
 // expose the public folder so it can be used.
 app.use(express.static('public'));
-
+// use body Parser middleware to get variables submitted (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
 //use EJS (no require needed)
 app.set('view engine', 'ejs')
 
@@ -27,6 +31,25 @@ app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
     res.render('index');
 });
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weather = JSON.parse(body)
+        if(weather.main == undefined){
+            //second argument is handed to view
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+  })
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
